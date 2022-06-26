fc014378c6d65122be59b4e92ff748b352c71ceb
Tellula <julieloane@gmail.com>
2022-06-26 15:53:08 +0200

    Adding weather widget

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,9 +1,38 @@
-import "./current-weather.css"
+import "./current-weather.css";
 
 const CurrentWeather = () => {
-    return (
-      "Hello"  
-    )
-}
+  return (
+    <div className="weather">
+      <div className="top">
+        <div className="">
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img src="icons/01d.png" alt="weather" className="weather-icon" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <p className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2m/sec</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15 %</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
