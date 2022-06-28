c4f6a12ce30a86445bff17eb20410f4d4c4e6933
Tellula <julieloane@gmail.com>
2022-06-29 01:25:03 +0200

    Insert checkbox

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { GEO_API_URL, geoApiOptions } from "../api";
-import "./search.css"
+import Checkbox from "./checkbox";
+import "./search.css";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+  const [citizens, setCitizens] = useState({ citizens: 1000000 });
 
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=${citizens.citizens}&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
@@ -30,15 +32,23 @@ const Search = ({ onSearchChange }) => {
     onSearchChange(searchData);
   };
 
+  const togglePreference = () => {
+    const value = citizens.citizens === 1000000 ? 20000 : 1000000;
+    setCitizens({ citizens: value });
+  };
+
   return (
-    <AsyncPaginate
-      className="searchBar"
-      placeholder="Search for a city..."
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      loadOptions={loadOptions}
-    />
+    <div>
+      <AsyncPaginate
+        className="searchBar"
+        placeholder="Search for a city..."
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
+      />
+      <Checkbox onChange={togglePreference} citizensNumber="20000" />
+    </div>
   );
 };
 
